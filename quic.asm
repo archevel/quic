e525e6effd70bcacb42ec1f70e86c9d4aaa2bf7d
Emil Hellman <emil.hellman@gmail.com>
2016-12-06 20:42:29 +0100

    Add license text to source

--- quic.asm
+++ quic.asm
@@ -1,3 +1,16 @@
+;; Quic - execute programs in quasi-isolated conatiners
+;; Copyright (C) 2016  Emil Hellman
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
 	STDIN equ 0
 	STDOUT equ 1
 	STDERR equ 2
